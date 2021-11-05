259ef7a59e12ad79de480ad0fb906aae1efd5666
RedDusty <zprobius@gmail.com>
2021-11-05 12:40:48 +0300

    update dep || set server

--- src/socketio.ts
+++ src/socketio.ts
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
+require('dotenv').config()
 
-const socket = io();
+const socket = io(process.env.REACT_APP_SERVER || '127.0.0.1:3000');
 
 export default socket;
