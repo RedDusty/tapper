20b0bc42ff5454d838abb3e32da67c68a09772ea
RedDusty <zprobius@gmail.com>
2021-11-14 23:35:03 +0300

    purge css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './purgeCSS.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import i18next from 'i18next';
